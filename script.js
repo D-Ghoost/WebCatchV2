99b662d7d4f51f38aa5156ff64a2f0241e3d350e
Nicolas-Ghost <natlopeztorres125@gmail.com>
2022-09-25 17:56:02 -0500

    Se regresa a una version anterior por cuestion de error

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName ("nav-container__toggle") [0]
-const navbarlinks = document.getElementsByClassName ("nav__articles") [0]
-
-toggleButton.addEventListener('click', () => {
-    navbarlinks.classList.toggle('active')
-})
